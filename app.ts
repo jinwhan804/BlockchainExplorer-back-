78ed0f42b571261a8e74e5c4f28d8dc954ff8524
jinwhan804 <jinwhan804@hanmail.net>
2023-11-22 10:32:51 +0900

    front test

--- app.ts
+++ app.ts
@@ -47,10 +47,10 @@ app.use(ErrorFn); // 예외처리 미들웨어
 app.listen(8080, async () => {
   console.log("server open");
 
-  if ((await getRPC_URLtest()) === "https://network.bouncecode.net/") {
-    CollectStart_http();
-  } else {
-    subscribetest();
-  }
+  // if ((await getRPC_URLtest()) === "https://network.bouncecode.net/") {
+  //   CollectStart_http();
+  // } else {
+  //   subscribetest();
+  // }
   console.log("테스트 구문 끝");
 });
