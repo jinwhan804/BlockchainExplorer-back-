0fe60f17dfd2fc222c0188cc871b63c76eeb8c16
gusdnr205 <gusdnr205@gmail.com>
2023-11-29 18:06:48 +0900

    createTokeneoa업데이트

--- TokenEoa/Token_EOA.service.ts
+++ TokenEoa/Token_EOA.service.ts
@@ -9,15 +9,31 @@ const createTokenEoa = async (
 ) => {
   try {
     console.log("createTokenEoa", tokenId, eoaId, amount, name, symbol);
-    await db.models.TokenEOA.create({
-      eoaid: eoaId,
-      tokenid: tokenId,
-      amount: amount,
-      name: name,
-      symbol: symbol,
+
+    // 중복된 값이 있는지 확인
+    const existingTokenEoa = await db.models.TokenEOA.findOne({
+      where: {
+        eoaid: eoaId,
+        tokenid: tokenId,
+      },
     });
+
+    if (!existingTokenEoa) {
+      // 중복된 값이 없으면 레코드 추가
+      await db.models.TokenEOA.create({
+        eoaid: eoaId,
+        tokenid: tokenId,
+        amount: amount,
+        name: name,
+        symbol: symbol,
+      });
+
+      console.log("TokenEOA created successfully.");
+    } else {
+      console.log("TokenEOA already exists. Not creating a duplicate.");
+    }
   } catch (error) {
-    console.log("createTokenEoa", error);
+    console.error("createTokenEoa", error);
   }
 };
 
