7b0e0e1085d398d96f52112b6698e5ddf716edeb
jinwhan804 <jinwhan804@hanmail.net>
2023-12-01 16:37:10 +0900

    설정 변경

--- dist/app.js
+++ dist/app.js
@@ -23,17 +23,13 @@ const EOA_router_1 = __importDefault(require("./EOA/EOA.router"));
 const CA_router_1 = __importDefault(require("./CA/CA.router"));
 const Event_log_router_1 = __importDefault(require("./Eventlog/Event_log.router"));
 const cors_1 = __importDefault(require("cors")); // cors 패키지 추가
-const CollectStart_http_1 = require("./information-system/function/CollectStart_http");
-const CollectStart_websocket_1 = require("./information-system/function/CollectStart_websocket");
-const config_1 = require("./information-system/function/config");
 const errorExcept_1 = require("./database/errorExcept");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    // origin: "https://bouncexplorer.site",
-    origin: "*",
+    origin: "https://bouncexplorer.site",
 }));
-database_1.default.sync({ force: true })
+database_1.default.sync({ force: false })
     .then(() => {
     console.log("connect on");
 })
@@ -53,18 +49,17 @@ app.use(errorExcept_1.ErrorFn); // 예외처리 미들웨어
 app.listen(8080, () => __awaiter(void 0, void 0, void 0, function* () {
     console.log("server open");
     // await saveABIandAddress();
-    yield (0, getnft_info_1.getnftinfo)();
+    // await getnftinfo();
     // await getTokeninfo();
     // await getEoainfo();
     // await getBlockInfo(3404791);
     // await getallblock();
     // await EOAService.findTxByEOA();
     // await getToken_by_user();
-    if ((yield (0, config_1.getRPC_URLtest)()) === "https://network.bouncecode.net/") {
-        (0, CollectStart_http_1.CollectStart_http)();
-    }
-    else {
-        (0, CollectStart_websocket_1.subscribetest)();
-    }
+    // if ((await getRPC_URLtest()) === "https://network.bouncecode.net/") {
+    //   CollectStart_http();
+    // } else {
+    //   subscribetest();
+    // }
     console.log("테스트 구문 끝");
 }));
