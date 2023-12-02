15ea5da0e5b3eaf4fd3179a519e8c30d252dbfd0
jinwhan804 <jinwhan804@hanmail.net>
2023-12-02 14:53:52 +0900

    typescript compile

--- dist/information-system/function/block/getAllblock.js
+++ dist/information-system/function/block/getAllblock.js
@@ -16,20 +16,25 @@ const config_1 = require("../config");
 const CHAIN_ID = 18328;
 const myQueue = new getQueue_1.Queue();
 const getallblock = () => __awaiter(void 0, void 0, void 0, function* () {
-    const web3 = yield (0, config_1.getProvider)();
-    const latestBlock = yield web3.eth.getBlock("latest", true);
-    console.log(latestBlock);
-    let latestBlockNumber;
-    if (latestBlock.number !== undefined) {
-        latestBlockNumber = latestBlock.number;
-        for (let i = 3404784; i <= Number(latestBlockNumber) - 1; i++) {
-            // console.log(i);
-            const block = yield web3.eth.getBlock(i);
-            yield (0, analyzeData_1.analyzeData)(block);
-            if (block.transactions) {
-                // console.log(block.transactions);
+    try {
+        const web3 = yield (0, config_1.getProvider)();
+        const latestBlock = yield web3.eth.getBlock("latest", true);
+        console.log(latestBlock);
+        let latestBlockNumber;
+        if (latestBlock.number !== undefined) {
+            latestBlockNumber = latestBlock.number;
+            for (let i = 3404784; i <= Number(latestBlockNumber) - 1; i++) {
+                yield new Promise((resolve) => setTimeout(resolve, 100)); // 1초 지연
+                const block = yield web3.eth.getBlock(i);
+                yield (0, analyzeData_1.analyzeData)(block);
+                if (block.transactions) {
+                    // console.log(block.transactions);
+                }
             }
         }
+    }
+    catch (error) {
+        console.log(error);
     }
 });
 exports.getallblock = getallblock;
